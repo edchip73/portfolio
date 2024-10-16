a43552d514f212c4946ab8e09fb9848bbd5794ca
Carlos Eduardo de Souza Santos <edchip73@gmail.com>
2024-10-16 11:44:05 -0300

    primeiro

---
+++ index.php
@@ -0,0 +1,389 @@
+<?php
+// Array multidimensional com informações dos cursos
+$certificados = [
+ 
+    [
+        "curso" => "Montagem/Manutenção",
+        "data_conclusao" => "14/15/1995",
+        "carga_horaria" => "64 horas",
+        "imagem" => "certificados/2-ICT_Montagem.png"
+    ],
+	[
+        "curso" => "Graduação",
+        "data_conclusao" => "01/10/1996",
+        "carga_horaria" => "3 Anos",
+        "imagem" => "certificados/1-Graduacao.png"
+    ],
+		  [
+        "curso" => "Rede Novell Netware 4.11",
+        "data_conclusao" => "07/08/1998",
+        "carga_horaria" => "40 horas",
+        "imagem" => "certificados/3-Impacta_Novell.png"
+    ],
+    [
+        "curso" => "Montagem/Manutenção",
+        "data_conclusao" => "25/08/2000",
+        "carga_horaria" => "40 horas",
+        "imagem" => "certificados/4-FIEPA_montagem.png"
+    ],
+
+	[
+        "curso" => "Linux Essentials",
+        "data_conclusao" => "08/05/2012",
+        "carga_horaria" => "74 horas",
+        "imagem" => "certificados/5-4Linux_Essentials.png"
+    ],
+	[
+        "curso" => "PHP - Orientação a Objetos",
+        "data_conclusao" => "05/11/2015",
+        "carga_horaria" => "40 horas",
+        "imagem" => "certificados/6-TreinaWeb_PrimeiroPHP.png"
+    ],
+	[
+        "curso" => "C# para Iniciantes",
+        "data_conclusao" => "09/09/2022",
+        "carga_horaria" => "15 horas",
+        "imagem" => "certificados/8-Udemy_ccrie4Projetos.png"
+    ],
+	[
+        "curso" => "C# Completo",
+        "data_conclusao" => "28/09/2022",
+        "carga_horaria" => "38 horas",
+        "imagem" => "certificados/7-Udemy_CCompleto.png"
+    ],
+	[
+        "curso" => "PHP Essencial",
+        "data_conclusao" => "05/11/2022",
+        "carga_horaria" => "16.5 horas",
+        "imagem" => "certificados/9-Udemy_PHPEssencial.png"
+    ],
+	[
+        "curso" => "HTML5 e CSS3 Essentials",
+        "data_conclusao" => "04/01/2023",
+        "carga_horaria" => "45 horas",
+        "imagem" => "certificados/8-Upinside_HTML5CSS3.png"
+    ],
+	[
+        "curso" => "JQuery Essentials",
+        "data_conclusao" => "08/01/2023",
+        "carga_horaria" => "35 horas",
+        "imagem" => "certificados/11-Upinside_Jquery.png"
+    ],
+	[
+        "curso" => "GIT Workflow",
+        "data_conclusao" => "10/01/2023",
+        "carga_horaria" => "15 horas",
+        "imagem" => "certificados/9-Upinside_GitWorkflow.png"
+    ],
+	[
+        "curso" => "Composer na Prática",
+        "data_conclusao" => "16/01/2023",
+        "carga_horaria" => "20 horas",
+        "imagem" => "certificados/14-Upinside_Composer.png"
+    ],
+	[
+        "curso" => "MariaDB Essentials",
+        "data_conclusao" => "23/01/2023",
+        "carga_horaria" => "30 horas",
+        "imagem" => "certificados/12-Upinside_MariaDB.png"
+    ],  
+	[
+        "curso" => "Web Master Business",
+        "data_conclusao" => "24/01/2023",
+        "carga_horaria" => "25 horas",
+        "imagem" => "certificados/10-upinside_WebBusiness.png"
+    ],
+	[
+        "curso" => "Web Design em Produção",
+        "data_conclusao" => "01/02/2023",
+        "carga_horaria" => "35 horas",
+        "imagem" => "certificados/7-Upinside_WebDesign.png"
+    ],
+	
+	[
+        "curso" => "CSS Produtivo com SASS",
+        "data_conclusao" => "02/02/2023",
+        "carga_horaria" => "10 horas",
+        "imagem" => "certificados/15-Upinside_SASS.png"
+    ],
+	[
+        "curso" => "Ambiente Local com Docker",
+        "data_conclusao" => "06/02/2023",
+        "carga_horaria" => "10 horas",
+        "imagem" => "certificados/17-Upinside_Docker.png"
+    ],
+	
+	[
+        "curso" => "Bootstrap Builder",
+        "data_conclusao" => "17/02/2023",
+        "carga_horaria" => "25 horas",
+        "imagem" => "certificados/13-Upinside_Bootstrap.png"
+    ],
+	[
+        "curso" => "Wordpress Essentials",
+        "data_conclusao" => "20/02/2023",
+        "carga_horaria" => "15 horas",
+        "imagem" => "certificados/16-Upinside_Wordpress.png"
+    ],
+	[
+        "curso" => "Aprenda PHP Sites Dinâmicos",
+        "data_conclusao" => "03/03/2023",
+        "carga_horaria" => "9.5 horas",
+        "imagem" => "certificados/34-Udemy_PHPSitesDinamicos.png"
+    ],		
+	
+	[
+        "curso" => "PHP - Fundamentos",
+        "data_conclusao" => "04/04/2023",
+        "carga_horaria" => "20 horas",
+        "imagem" => "certificados/22-TreinaWeb_PHP_Fundamentos.png"
+    ],	
+	[
+        "curso" => "PHP - Orientação a Objetos 1",
+        "data_conclusao" => "07/04/2023",
+        "carga_horaria" => "16 horas",
+        "imagem" => "certificados/24-TreinaWeb_PHP_OO1.png"
+    ],	
+	[
+        "curso" => "Ubuntu Linux",
+        "data_conclusao" => "14/04/2023",
+        "carga_horaria" => "14 horas",
+        "imagem" => "certificados/29-TreinaWeb_Ubuntu.png"
+    ],		
+	[
+        "curso" => "PHP - Orientação a Objetos 2",
+        "data_conclusao" => "14/04/2023",
+        "carga_horaria" => "18 horas",
+        "imagem" => "certificados/25-TreinaWeb_PHP_OO2.png"
+    ],	
+
+	[
+        "curso" => "PHP do Zero a Maestria",
+        "data_conclusao" => "15/04/2022",
+        "carga_horaria" => "33 horas",
+        "imagem" => "certificados/10-Udemy_PHPZeroAMaestria.png"
+    ],
+	[
+        "curso" => "PHP - Recursos Essenciais",
+        "data_conclusao" => "17/04/2023",
+        "carga_horaria" => "16 horas",
+        "imagem" => "certificados/27-TreinaWeb_PHP_Recursos.png"
+    ],	
+	[
+        "curso" => "Samba - Servidores de Arquivos",
+        "data_conclusao" => "19/04/2023",
+        "carga_horaria" => "10 horas",
+        "imagem" => "certificados/28-TreinaWeb_Samba.png"
+    ],		
+	[
+        "curso" => "Novidades do PHP 8.0",
+        "data_conclusao" => "20/04/2023",
+        "carga_horaria" => "08 horas",
+        "imagem" => "certificados/23-TreinaWeb_PHP_Novidades8.png"
+    ],	
+	[
+        "curso" => "Full Stack PHP Developer",
+        "data_conclusao" => "25/04/2023",
+        "carga_horaria" => "200 horas",
+        "imagem" => "certificados/18-Upinside_PHPFullStack.png"
+    ],
+	[
+        "curso" => "Banco de Dados - Fundamentos",
+        "data_conclusao" => "26/05/2023",
+        "carga_horaria" => "40 horas",
+        "imagem" => "certificados/26-TreinaWeb_PHP_ProjetoBancoDados.png"
+    ],	
+	[
+        "curso" => "MySQL - Desenvolvedor",
+        "data_conclusao" => "31/05/2023",
+        "carga_horaria" => "24 horas",
+        "imagem" => "certificados/19-TreinaWeb_MySQL.png"
+    ],
+	[
+        "curso" => "PHP - Banco de Dados",
+        "data_conclusao" => "01/06/2023",
+        "carga_horaria" => "08 horas",
+        "imagem" => "certificados/20-TreinaWeb_PHP_BancoDeDados.png"
+    ],	
+	[
+        "curso" => "PHP - Composer",
+        "data_conclusao" => "03/06/2023",
+        "carga_horaria" => "08 horas",
+        "imagem" => "certificados/21-TreinaWeb_PHP_Composer.png"
+    ],	
+    [
+        "curso" => "Iniciando PHP OO",
+        "data_conclusao" => "30/10/2023",
+        "carga_horaria" => "08 horas",
+        "imagem" => "certificados/33-Udemy_PHPOO.png"
+    ],
+	[
+        "curso" => "Big Data - Introdução",
+        "data_conclusao" => "25/11/2023",
+        "carga_horaria" => "14 horas",
+        "imagem" => "certificados/30-Impacta_BigData.png"
+    ],	
+	[
+        "curso" => "PHP 7 com OO",
+        "data_conclusao" => "29/11/2023",
+        "carga_horaria" => "10 horas",
+        "imagem" => "certificados/31-Impacta_PHP7OO.png"
+    ],	
+	[
+        "curso" => "DigitalDoc - PHP & MySQL",
+        "data_conclusao" => "06/01/2024",
+        "carga_horaria" => "6.5 horas",
+        "imagem" => "certificados/32-Udemy_DigitalDOC.png"
+    ],	
+	[
+        "curso" => "Fundamentos Obrigatórios",
+        "data_conclusao" => "17/05/2024",
+        "carga_horaria" => "07 horas",
+        "imagem" => "certificados/36-BEER_Fundamentos.png"
+    ],	
+	[
+        "curso" => "PHP para o Laravel",
+        "data_conclusao" => "25/05/2024",
+        "carga_horaria" => "03 horas",
+        "imagem" => "certificados/37-BEER_PHPLaravel.png"
+    ],	
+
+	[
+        "curso" => "Boas Práticas no Laravel",
+        "data_conclusao" => "26/05/2024",
+        "carga_horaria" => "1 hora",
+        "imagem" => "certificados/35-BEER_BoasPraticas.png"
+    ],	
+	[
+        "curso" => "Desenvolvimento Moderno",
+        "data_conclusao" => "13/06/2024",
+        "carga_horaria" => "02 horas",
+        "imagem" => "certificados/47-DIO_Moderno.png"
+    ],		
+	[
+        "curso" => "Ecosistema e Documentação",
+        "data_conclusao" => "18/06/2024",
+        "carga_horaria" => "02 horas",
+        "imagem" => "certificados/43-DIO_Ecosistema.png"
+    ],	
+	[
+        "curso" => "Tipos de Variáveis",
+        "data_conclusao" => "19/06/2024",
+        "carga_horaria" => "1 hora",
+        "imagem" => "certificados/45-DIO_Tipos.png"
+    ],	
+	[
+        "curso" => "Introdução a Programação",
+        "data_conclusao" => "19/06/2024",
+        "carga_horaria" => "05 horas",
+        "imagem" => "certificados/40-DIO_Introducao.png"
+    ],		
+	[
+        "curso" => "Introdução a Banco de Dados",
+        "data_conclusao" => "20/06/2024",
+        "carga_horaria" => "03 horas",
+        "imagem" => "certificados/39-DIO_BancoDeDados.png"
+    ],	
+	[
+        "curso" => "Fundamentos de DEV",
+        "data_conclusao" => "20/06/2024",
+        "carga_horaria" => "14 horas",
+        "imagem" => "certificados/41-DIO_Fundamentos.png"
+    ],	
+	[
+        "curso" => "Estruturas Condicionais",
+        "data_conclusao" => "20/06/2024",
+        "carga_horaria" => "03 horas",
+        "imagem" => "certificados/44-DIO_Estruturas.png"
+    ],	
+	
+	[
+        "curso" => "Gerenciamento - Composer",
+        "data_conclusao" => "23/06/2024",
+        "carga_horaria" => "02 horas",
+        "imagem" => "certificados/46-DIO_Composer.png"
+    ],
+	[
+        "curso" => "Debub e Tratamento",
+        "data_conclusao" => "23/06/2024",
+        "carga_horaria" => "1 hora",
+        "imagem" => "certificados/42-DIO_Debug.png"
+    ],	
+	[
+        "curso" => "Tratamento de Exceções",
+        "data_conclusao" => "24/06/2024",
+        "carga_horaria" => "07 horas",
+        "imagem" => "certificados/49-DIO_Tratamento.png"
+    ],	
+	[
+        "curso" => "Programação OO em PHP",
+        "data_conclusao" => "24/06/2024",
+        "carga_horaria" => "03 horas",
+        "imagem" => "certificados/50-DIO_OOPHP.png"
+    ],	
+
+	[
+        "curso" => "Orientação a Objetos",
+        "data_conclusao" => "25/06/2024",
+        "carga_horaria" => "03 horas",
+        "imagem" => "certificados/48-DIO_OOBD.png"
+    ],	
+	[
+        "curso" => "PHP Programando com OO",
+        "data_conclusao" => "19/07/2024",
+        "carga_horaria" => "30 horas",
+        "imagem" => "certificados/38-Adianti_PHPOO.png"
+    ],			
+	[
+        "curso" => "Formação PHP Experience",
+        "data_conclusao" => "06/10/2024",
+        "carga_horaria" => "40 horas",
+        "imagem" => "certificados/51-DIO_FormacaoPHP.png"
+    ],		
+	[
+        "curso" => "Curso PHP Rocketseat",
+        "data_conclusao" => "11/10/2024",
+        "carga_horaria" => "5 horas",
+        "imagem" => "certificados/52-Rocketseat_PHPOnLine.png"
+    ],		
+		
+	
+    // Adicione mais certificados conforme necessário
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<link rel="icon" type="image/png" href="img/icone.png">
+	<link rel="stylesheet" href="css/style.css" />
+    <title>Portfólio de Certificados</title>
+    
+</head>
+<body class="bg-slate-800 text-white" oncontextmenu="return false">
+<header class="bg-slate-900 mx-auto max-w-screen-lg px-3 py-6">
+<h1 class="title1">Portfólio de Certificados</h1>
+
+<h3 class="title2">Carlos Eduardo de Souza Santos</h3>
+
+</header>
+<?php foreach ($certificados as $certificado): ?>
+
+    <div class="certificado">
+        
+        <div class="info">
+            <strong><?php echo $certificado['curso']; ?></strong><br>
+            Conclusão: <?php echo $certificado['data_conclusao']; ?><br>
+            Carga Horária: <?php echo $certificado['carga_horaria']; ?>
+        </div class="p-8">
+		<img class="w-16 md:w-32 lg:w-48" src="<?php echo $certificado['imagem']; ?>" alt="<?php echo $certificado['curso']; ?>">
+    </div>
+
+<?php endforeach; ?>
+
+</body>
+</html>
